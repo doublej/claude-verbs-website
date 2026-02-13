2d77ed1acdca16e9f3afe4872988f4d83943b51e
JJ <jurrejan@users.noreply.github.com>
2026-02-13 12:43:59 +0000

    fix: adjust mobile scale to 0.6

--- src/lib/pixi/app.ts
+++ src/lib/pixi/app.ts
@@ -414,7 +414,7 @@ export async function createApp(
     // Shift focus area to the left on mobile to prevent content from being off-screen
     params.absoluteX = 2
     // Scale back the scene on mobile to fit better
-    params.scale = 0.7
+    params.scale = 0.6
     demoCleanup = initMobileDemo({
       machine,
       localeSets,
